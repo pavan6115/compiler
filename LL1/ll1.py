fdcf55ee711bc1b6aa92c5045f36fb268418c484
kpavann <kulkarnipavan6115@gmail.com>
2021-04-25 23:14:40 +0530

    code file of LL(1)

--- LL1/ll1.py
+++ LL1/ll1.py
@@ -10,11 +10,18 @@ Rules of follow -->
 1. First(S) - A but A is non terminal or variable -> First(A) -> {a, @} 	
 2. First(A) - terminal,epsilon
 
+Rules for LL(1) Table --> 
+1. Number the production as 1 2 3 and so on. They are production numbers
+2. For table, select the non-terminal and find its first and fill the production number in the resp column. 
+3. If one of the production starts with ε then the write the production number in corresponding non-terminal's follow
+
 Using bottom-up approach for finding first 
 Using top-down approach for finding follow
 """
 
 import re
+from prettytable import PrettyTable 
+
 reg1 = "(?:[*+()@]|[a-z])"
 reg2 = "(id)"
 reg3 = "(?:[*+()]|[a-z])"
@@ -74,7 +81,8 @@ def follow(nTKeys, prodDict, firstDict):
             followDict[nT].add('$') 
     return followDict
 
-        
+
+
 
 if __name__ == "__main__":
     with open('input.txt', 'r') as f:
@@ -86,24 +94,50 @@ if __name__ == "__main__":
         nT, rhs = production.split(' --> ')
         prodDict[nT] = rhs.split(' | ')
 
+    rhs = [] # for LL(1)
     for nT in prodDict:
+        rhs.append(prodDict[nT])
         print(nT, ':', prodDict[nT])
     
     nTKeys = list(prodDict)
     
-    #return values of first to be stored in a dict
+    # return values of first to be stored in a dict
     firstDict={}
     firstDict = first(nTKeys, prodDict)
     print('First ---->')
     for keys in nTKeys:
         print(keys, ':', firstDict[keys])
     
-    #returns values of follow (dict)
+    # returns values of follow (dict)
     followDict = follow(nTKeys, prodDict, firstDict)
     print('Follow ---->')
     for key in nTKeys:
+        # terminalSet.add(followDict[key])
         print(key, ':', followDict[key])
+
+    # LL(1) Parser
+    print('nonterminal keys', nTKeys)
+    print('rhslist - ',rhs)
+
+    terminal = []
+    for r in rhs:
+        for e in r:
+            if re.match(reg2, e):
+                terminal.append(e)
+                continue
+            for j in e:
+                if re.match(reg1, j):
+                    terminal.append(j)
+    print('terminals - ',terminal)
+
+
+    myTable = PrettyTable(['nT/T'] + terminal)
+
+
+
+    print(myTable)
+
+
     
-
-
+    
 
